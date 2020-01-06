b894edccd9ed7d75276341a6d7b227896e912933
Zaza Saki <isac.cruzn@gmail.com>
2020-01-06 13:35:08 +0000

    NavMesh, Click and point Test, AI Controller Scheme

---
+++ NWTL_LIAI/Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{   
+    public Camera cam;  
+
+    public NavMeshAgent agent;
+
+    private Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetMouseButtonDown(0))
+       {
+           Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+           RaycastHit hit;
+
+           if (Physics.Raycast(ray, out hit))
+           {
+               agent.SetDestination(hit.point);
+           }
+
+       }
+    }
+
+
+}
