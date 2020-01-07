0cb9f4af7e43e3b3677c149d0b89402301d4d6e2
Zaza Saki <isac.cruzn@gmail.com>
2020-01-07 15:51:42 +0000

    Behavior AI, EnemieParent

---
+++ NWTL_LIAI/Assets/Scripts/AI/AIBehavior.cs
@@ -0,0 +1,60 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBehavior : MonoBehaviour{   
+
+    public NavMeshAgent Agent;
+//Black Board
+    //Player vars
+    Vector3 playerLoc;
+    bool playerIsFiring;
+    bool playerIsRunning;
+
+    //Base vars
+    Vector3 baseLoc;
+    bool baseIsActive;
+
+
+//Behavior
+    
+    // Update is called once per frame
+    void Update()
+    {
+        FindPlayer();
+        ChasePlayer();
+    }
+
+    public void FindPlayer(){
+        playerLoc = FindObjectOfType<PlayerMovement>().GetComponent<Transform>().position;
+        Debug.Log("Player Found : " + playerLoc);
+    }
+
+    public void FindActiveBase(){
+        
+    }
+
+    public bool IsDetected(){
+        if (true)
+        {
+            
+        }
+        return false;
+    }
+
+    public void ChasePlayer(){
+        Agent.SetDestination(playerLoc);
+    }
+
+    public void attack(){
+        GetEnemieParentScript().attack();
+    }
+
+    public EnemieParentScript GetEnemieParentScript(){
+        return GetComponent<EnemieParentScript>();
+    }
+
+    public Transform GetTransform(){
+        return GetComponent<Transform>();
+    }
+}
---
+++ NWTL_LIAI/Assets/Scripts/AI/EnemieParentScript.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class EnemieParentScript : MonoBehaviour
+{   
+    public float attackRange;
+    public float detectionRunRange;
+    public float detectionShootRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void attack(){
+        Debug.Log("attack");
+    }
+}
