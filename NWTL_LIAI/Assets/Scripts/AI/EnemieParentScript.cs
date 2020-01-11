f2169a27f251998bce56198e90012a18e6588af1
Zaza Saki <isac.cruzn@gmail.com>
2020-01-11 12:46:19 +0000

    Spawn Manager, EnemieParentScript(Stats)
    Generate enemie

--- NWTL_LIAI/Assets/Scripts/AI/EnemieParentScript.cs
+++ NWTL_LIAI/Assets/Scripts/AI/EnemieParentScript.cs
@@ -1,11 +1,35 @@
 ﻿
 using UnityEngine;
 
-public class EnemieParentScript : MonoBehaviour
-{   
+public class EnemieParentScript : MonoBehaviour{
+
+    public enum Type
+    {
+        Fast, Thank, Balanced
+    }
+
+    public enum Size
+    {
+        Small, Med, Big
+    }
+
+    public int level;
+    public Type type;
+    public Size size;
+
+
     public float attackRange;
 
     public void attack(){
         Debug.Log("attack");
     }
+
+    
+
+    //Check Stats Function
+    public bool checkedStats(int l, Type t, Size s){return l == level && t.Equals(type) && s.Equals(size);}
+    public bool checkedStats(int l, Type t){return l == level && t.Equals(type);}
+    public bool checkedStats(Size s){return s.Equals(size);}
+    public bool checkedStats(int l){ return l == level;}
+    public bool checkedStats(Type t){return t.Equals(type);}
 }
--- NWTL_LIAI/Assets/Scripts/AI/SpawnManager.cs
+++ NWTL_LIAI/Assets/Scripts/AI/SpawnManager.cs
@@ -2,15 +2,64 @@
 using UnityEngine.AI;
 
 public class SpawnManager : MonoBehaviour{
-
-    public SpawnInfo[] List;
+    private int SpawnLevel;
+    private EnemieParentScript.Type SpawnType;
+    public GameObject[] List;
     public NavMeshSurface nav;
-    public GameObject AI_Tester;
+    GameObject ObjectToSpawn;
 
     // Start is called before the first frame update
+    private void Start() {
+        foreach (GameObject item in List)
+        {
+            if(item.GetComponent<EnemieParentScript>()==null)
+                Debug.Log("There Is A Non Enemie Game Object In The Spawn List");
+        }
+
+        
+    }
+    
+    
+    public void CalculateEnemie(float Level1Prob, float Level2Prob, float TypeFastprb, float TypeBalancedProb){
+        System.Random rand = new System.Random();
+        float prob = (float)rand.NextDouble() * 100f;
+        
+        //Generating Level
+        if (prob <= Level1Prob){
+            SpawnLevel = 1;
+        }else if (prob <= Level2Prob + Level1Prob){
+            SpawnLevel = 2;
+        }else{
+            SpawnLevel = 3;
+        }
+
+        //Generating Type
+        prob = (float)rand.NextDouble();
+        
+        if (prob <= TypeFastprb){
+            SpawnType = EnemieParentScript.Type.Fast;
+        }else if (prob <= TypeFastprb + TypeBalancedProb){
+            SpawnType = EnemieParentScript.Type.Balanced;
+        }else{
+            SpawnType = EnemieParentScript.Type.Thank;
+        }
+
+
+        //Search for the game object with the correct stats
+        foreach (GameObject item in List)
+        {
+            if(item.GetComponent<EnemieParentScript>().checkedStats(SpawnLevel, SpawnType)){
+                ObjectToSpawn = item;
+            }else
+            {
+                CalculateEnemie(Level1Prob, Level2Prob, TypeFastprb, TypeBalancedProb);
+            }
+        }
+    }
+    
     private void spawn(Vector3 spawnLoc){
         spawnLoc = nav.navMeshData.sourceBounds.ClosestPoint(spawnLoc);
-        Instantiate(AI_Tester, spawnLoc,  new Quaternion());
+        Instantiate(ObjectToSpawn, spawnLoc,  new Quaternion());
         
         
     }
