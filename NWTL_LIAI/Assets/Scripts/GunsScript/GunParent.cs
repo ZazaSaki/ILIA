7794adca7ddb8a81515947f9c4797b95369f457a
Zaza Saki <isac.cruzn@gmail.com>
2020-09-21 22:32:57 +0100

    KeyHandler Interfaces System

--- NWTL_LIAI/Assets/Scripts/GunsScript/GunParent.cs
+++ NWTL_LIAI/Assets/Scripts/GunsScript/GunParent.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class GunParent : MonoBehaviour
+public abstract class GunParent : MonoBehaviour, IFireble
 {   //Mesh
     public string[] Mesh; 
     public string id;
@@ -59,5 +59,15 @@ public abstract class GunParent : MonoBehaviour
 
     protected bool IsEnoughAmmo(){
         return !(WeaponHolder.Ammo < AmmoByShot);
+    }
+
+    public void selfAssin(KeyHandler kh)
+    {
+        kh.add(this);
+    }
+
+    void IKeyHandable.id()
+    {
+        
     }
 }
