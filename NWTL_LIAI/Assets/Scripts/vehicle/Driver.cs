e86f9b91b1cd67f1ecb35970d537dc232eb04b6a
Zaza Saki <isac.cruzn@gmail.com>
2020-10-14 13:30:50 +0100

    Vehicle
    Building the hover physics, driver physics, and test map

---
+++ NWTL_LIAI/Assets/Scripts/vehicle/Driver.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : Tool, IMovable
+{
+    Rigidbody rb;
+    public float moveForce;
+    public float torqueForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelfMove(float HorizontalAxis, float VerticalAxis){
+        rb.AddForce(VerticalAxis*transform.forward*moveForce);
+        rb.AddTorque(HorizontalAxis*transform.up*torqueForce);
+    }
+    public void jump(){
+    }
+
+    public void sprint(){
+        
+    }
+
+    public void walk(){
+        
+    }
+
+    public void id(){Debug.Log("driving....");}
+    public void selfAssin(KeyHandler kh){kh.add(this);}
+
+    public override void Action()
+    {
+        Debug.Log("selfassin");
+        //selfAssin(comp.GetComponent<KeyHandler>());
+    }
+}
---
+++ NWTL_LIAI/Assets/Scripts/vehicle/HoverPhysics.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverPhysics : MonoBehaviour
+{   
+
+    //hover vars
+    public float hoverForce;
+    public Transform[] hoverPoints = new Transform[4];
+    public RaycastHit[] hoverRays = new RaycastHit[4];
+    Rigidbody rb;
+
+
+    //motor vars
+    private float usableForce = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hoverPoints.Length; i++)
+        {
+            ApplyHoverForce(hoverPoints[i], hoverRays[i]);
+        }
+    }
+
+    private float CalculateHoverForce(Transform hoverPoint, RaycastHit hoverRay){
+        if (Physics.Raycast(hoverPoint.position, -hoverPoint.up, out hoverRay)){
+            
+            //Force multiplier value per HoverPoint
+            float multiplier = (1/(Mathf.Abs(hoverRay.point.y - hoverPoint.position.y)))/hoverPoints.Length;
+            return multiplier*hoverForce;
+            //aplying force
+            
+        }
+
+        return 0;
+    }
+
+    private void ApplyHoverForce(Transform hoverPoint, RaycastHit hoverRay){
+        usableForce = CalculateHoverForce(hoverPoint, hoverRay);
+        
+        rb.AddForceAtPosition(hoverPoint.up*usableForce, hoverPoint.position, ForceMode.Acceleration);
+    }
+}
