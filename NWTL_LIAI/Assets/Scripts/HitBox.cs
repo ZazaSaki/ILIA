ed0a7d822061b7d4f0b569c29c38114bac893271
Zaza Saki <isac.cruzn@gmail.com>
2020-10-14 13:29:24 +0100

    Debugging gun drops
    Modifying the gun drops to work with the new interface Activatable

---
+++ NWTL_LIAI/Assets/Scripts/HitBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{   
+    private IActivatable Item;
+    private void Start() {
+        Item = GetComponentInParent<IActivatable>();
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.GetComponent<PlayerInteractor>() != null){
+            other.GetComponent<PlayerInteractor>().setInteract(Item);
+            Item.setComponent(other.GetComponent<Transform>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+       if(other.transform.GetComponent<PlayerInteractor>() != null){
+           other.transform.GetComponent<PlayerInteractor>().resetInteract();
+           Item.setComponent(null);
+        }
+    }
+}
